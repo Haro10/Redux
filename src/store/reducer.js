59729170064ffb18be67f92ed4173800429c426c
Ha Kieu Minh <minhha19122014@gmail.com>
2019-02-11 23:23:57 +0900

    passing and retrieving data with action

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -8,6 +8,21 @@ const reducer = (state = initialState, action) => {
         counter: state.counter + 1 //update counter in state
         }
     }
+    if (action.type === 'DECREMENT') {
+        return { // just see only counter but it return all state
+        counter: state.counter - 1 //update counter in state
+        }
+    }
+    if(action.type === 'ADD'){
+        return { // just see only counter but it return all state
+        counter: state.counter + 5 //update counter in state
+        }
+    }
+    if(action.type === 'SUB'){
+        return { // just see only counter but it return all state
+        counter: state.counter - 5 //update counter in state
+        }
+    }
     return state;
 };
 
